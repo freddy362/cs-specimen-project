d308dade54791d8f41eee0fa279ce820bf0ca70c
Fred Tome-Govaert <ftgovaert@emanuel.org.uk>
2016-11-04 15:05:53 +0000

    finished displaying total mark per student

--- basicstats1.py
+++ basicstats1.py
@@ -1,3 +1,14 @@
 import cPickle as pickle
 tablenames = pickle.load(open('tablenames.p', 'rb'))
-print tablenames
+tablemark1 = pickle.load(open('tablemark1.p', 'rb'))
+tablemark2 = pickle.load(open('tablemark2.p', 'rb'))
+tablemark3 = pickle.load(open('tablemark3.p', 'rb'))
+totalmark = []
+amountofstudents = len(tablenames)
+for i in range(amountofstudents):
+    totalmark.append(tablemark1[i] + tablemark2[i] + tablemark3[i])
+    print tablenames[i]
+    print totalmark[i]
+    pickle.dump(totalmark, open("totalmark.p", "wb"))
+
+
