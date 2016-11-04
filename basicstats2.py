b7e071b4f3c1c395bccf526897351f0faa7c1ec0
Fred Tome-Govaert <ftgovaert@emanuel.org.uk>
2016-11-04 15:21:34 +0000

    finished high scores programming

--- basicstats2.py
+++ basicstats2.py
@@ -5,5 +5,15 @@ tablemark2 = pickle.load(open('tablemark2.p', 'rb'))
 tablemark3 = pickle.load(open('tablemark3.p', 'rb'))
 totalmark = pickle.load(open('totalmark.p', 'rb'))
 amountofstudents = len(tablenames)
+highscore = -65
+highscorers = []
 for i in range(amountofstudents):
+    if totalmark[i] >= highscore:
+        highscorers.append(tablenames[i])
+        highscore = totalmark[i]
+    else:
+        print 'Highscorers: ', highscorers
+    
+    
+    
     
