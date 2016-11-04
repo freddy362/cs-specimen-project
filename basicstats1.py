b95fefc3b63d2ffe2b17b258b59dba3888a9e23b
Fred Tome-Govaert <ftgovaert@emanuel.org.uk>
2016-11-04 15:13:15 +0000

    finished basic statistics code + debug

--- basicstats1.py
+++ basicstats1.py
@@ -4,11 +4,19 @@ tablemark1 = pickle.load(open('tablemark1.p', 'rb'))
 tablemark2 = pickle.load(open('tablemark2.p', 'rb'))
 tablemark3 = pickle.load(open('tablemark3.p', 'rb'))
 totalmark = []
+average = 0
 amountofstudents = len(tablenames)
 for i in range(amountofstudents):
     totalmark.append(tablemark1[i] + tablemark2[i] + tablemark3[i])
-    print tablenames[i]
-    print totalmark[i]
+    print tablenames[i],'got',totalmark[i],'overall'
     pickle.dump(totalmark, open("totalmark.p", "wb"))
+for i in range(amountofstudents):
+    average = tablemark1[i] + average
+    average = tablemark2[i] + average
+    average = tablemark3[i] + average
+print 'The average is' ,average
+pickle.dump(average, open("average.p", "wb"))
+
+
 
 
